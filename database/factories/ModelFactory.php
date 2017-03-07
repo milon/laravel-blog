0fcd13402513f179e8054b8c93489c6b4e34c8f1
Nuruzzaman Milon <contact@milon.im>
2017-03-07 20:48:01 +0600

    added model factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,24 +1,49 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
+use App\User;
+use App\Models\Tag;
+use App\Models\Post;
+use Faker\Generator;
+use App\Models\Comment;
+use App\Models\Category;
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(User::class, function (Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
+        'password'       => $password ?: $password = bcrypt('password'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(Post::class, function (Generator $faker) {
+    return [
+        'title'        => $faker->sentence,
+        'body'         => $faker->paragraph(3),
+        'user_id'      => rand(1, 10),
+        'category_id'  => rand(1, 10),
+        'is_published' => rand(0, 1)
+    ];
+});
+
+$factory->define(Category::class, function (Generator $faker) {
+    return [
+        'name' => $faker->word
+    ];
+});
+
+$factory->define(Tag::class, function (Generator $faker) {
+    return [
+        'name' => $faker->word
+    ];
+});
+
+$factory->define(Comment::class, function (Generator $faker) {
+    return [
+        'user_id' => rand(1, 10),
+        'post_id' => rand(1, 25),
+        'body'    => $faker->paragraph
     ];
 });
